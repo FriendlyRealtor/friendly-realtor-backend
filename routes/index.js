d64294985d6011e229dfa0c7b6c1122b929240cb
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-02-21 17:57:55 -0500

    handle nodejs express

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,25 @@ var router = express.Router();
 const axios = require("axios");
 
 router.get('/crm', function(req, res, next) {
-	const options = {
-		method: 'GET',
-		url: 'https://zillow56.p.rapidapi.com/property',
-		params: {zpid: req.body.id},
-		headers: {
-			'X-RapidAPI-Key': '983ea19bf5msh0178fe7715948dcp11b14ajsnadb182e8da0a',
-			'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
-		}
-	};
+	const { param } = req.query
 
-	axios.request(options).then(function (response) {
-		res.send(response.data)
-	}).catch(function (error) {
-		res.send(error)
-	});
+	if (parseInt(param) > 0) {
+		const options = {
+			method: 'GET',
+			url: 'https://zillow56.p.rapidapi.com/property',
+			params: {zpid: parseInt(param)},
+			headers: {
+				'X-RapidAPI-Key': '983ea19bf5msh0178fe7715948dcp11b14ajsnadb182e8da0a',
+				'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
+			}
+		};
+		axios.request(options).then(function (response) {
+			const { zestimate } = response.data;
+			res.send({ value: zestimate })
+		}).catch(function (error) {
+			res.send(error)
+		});
+	}
 });
 
 module.exports = router;
