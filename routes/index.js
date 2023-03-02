7f335fa2777fbcdafb3a88a0891f4957f945e84d
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-03-02 18:24:01 -0500

    Add google maps search

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,40 @@ var router = express.Router();
 const axios = require("axios");
 
 router.get('/crm', function(req, res, next) {
-	const { param } = req.query
+	const { location } = req.query
 
-	if (parseInt(param) > 0) {
-		const options = {
-			method: 'GET',
-			url: 'https://zillow56.p.rapidapi.com/property',
-			params: {zpid: parseInt(param)},
-			headers: {
-				'X-RapidAPI-Key': '983ea19bf5msh0178fe7715948dcp11b14ajsnadb182e8da0a',
-				'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
-			}
-		};
-		axios.request(options).then(function (response) {
-			const { zestimate } = response.data;
-			res.send({ value: zestimate })
-		}).catch(function (error) {
-			res.send(error)
-		});
-	}
+	const options = {
+		method: 'GET',
+		url: 'https://zillow56.p.rapidapi.com/search',
+		params: {location: location},
+		headers: {
+			'X-RapidAPI-Key': '983ea19bf5msh0178fe7715948dcp11b14ajsnadb182e8da0a',
+			'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
+		}
+	};
+	axios.request(options).then(function (response) {
+		const { zestimate } = response.data;
+		res.send({ value: zestimate })
+	}).catch(function (error) {
+		res.send(error)
+	});
 });
+
+router.get('/local-restaurants', function (req, res) {
+	const { location } = req.query;
+
+	// Radius here is in meters 8046.42 meters = 5 miles
+  const options = {
+		method: 'GET',
+		url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+		params: {location, radius: 8047, key: 'AIzaSyCGKfspL5CDIHVlz5C1iYiUWJcSiKWPUeM', business_status: 'OPERATIONAL', types: 'restaurant'},
+	};
+	axios.request(options).then(function (response) {
+		const { data } = response;
+		res.send(data)
+	}).catch(function (error) {
+		res.send(error)
+	});
+})
 
 module.exports = router;
